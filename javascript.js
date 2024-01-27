40d7920ee50bcdf051ff2ed8320255e2c339d40f
nren07 <imnren07@gmail.com>
2024-01-27 20:49:17 +0530

    home page with start solving popup window functionality

---
+++ javascript.js
@@ -0,0 +1,212 @@
+let quizData = [
+    {
+        "category": "General Knowledge",
+        "questions": [
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            }
+        ]
+    },
+
+    {
+        "category": "Physics",
+        "questions": [
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            }
+        ]
+    },
+
+    {
+        "category": "Maths",
+        "questions": [
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            }
+        ]
+    },
+
+    {
+        "category": "Chemistry",
+        "questions": [
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            }
+        ]
+    },
+    {
+        "category": "History",
+        "questions": [
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            }
+        ]
+    },
+    {
+        "category": "Geography",
+        "questions": [
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            },
+            {
+                "question": "Who wrote 'To Kill a Mockingbird'?",
+                "options": ["Harper Lee", "George Orwell", "Jane Austen", "F. Scott Fitzgerald"],
+                "answer": "Harper Lee"
+            }
+        ]
+    },
+];
+
+function popup_window(button){
+    
+    const ele = document.getElementById("popup");
+    let mainPage=document.getElementById("main_page");
+    mainPage.classList.toggle("main_page_class");
+    ele.classList.toggle("hide");
+    ele.classList.toggle("show");
+    // button.disabled=true;
+    
+}
+function close(){
+    console.log("event");
+    // let ele = document.getElementById("cross");
+    // let mainPage=document.getElementById("main_page");
+    // mainPage.classList.toggle("main_page_class");
+    // ele.classList.toggle("show");
+    // ele.classList.toggle("hide");
+    // button.disabled=false;
+}
+// let container=document.getElementsByClassName("categories");
+// for(let x in quizData){
+//     let ele=document.createElement("div");
+//     console.log(ele.innerHTML);
+//     container.append(ele);
+// }
+
